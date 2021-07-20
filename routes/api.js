dcf37650e99b762116ad2db9240173c2f1758ed8
Nicholas Brown <brown.nicholasj@gmail.com>
2021-07-19 19:57:56 -0500

    trying to get apis and seeding working, seem to have issue connecting to mongo

---
+++ routes/api.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const Workout = require('../models/workout.js');
+
+router.post('/api/transaction', ({ body }, res) => {
+	Workout.create(body)
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.status(400).json(err);
+		});
+});
+
+router.post('/api/transaction/bulk', ({ body }, res) => {
+	Workout.insertMany(body)
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.status(400).json(err);
+		});
+});
+
+router.get('/exercise', (req, res) => {
+	Workout.find({})
+		.sort({ date: -1 })
+		.then((dbWorkout) => {
+			res.render('exercise');
+		})
+		.catch((err) => {
+			res.status(400).json(err);
+		});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const logger = require('morgan');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(logger('dev'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,7 +19,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
 });
 
 // routes
-app.use(require('./public/api.js'));
+app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => {
 	console.log(`App running on port ${PORT}!`);
